bfc46b4428f9b8a41b2e913d3f894d61b5d709f7
ProstoVitya <sovsemnevitya@gmail.com>
2022-01-30 20:17:49 +0400

    Mechamisms
    Updated ammo prefabs
    Added a button prefab and a door prefab

---
+++ Assets/Scripts/Mechanisms Scripts/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Button : MonoBehaviour
+{
+    public UnityEvent OnButtonStay;
+
+    [Header("Optional")]
+    public UnityEvent OnButtonLeave;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.localPosition += new Vector3(0f, -0.1f, 0f);
+        OnButtonStay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (OnButtonLeave != null)
+            OnButtonLeave.Invoke();
+        transform.localPosition += new Vector3(0f, 0.1f, 0f);
+    }
+}
---
+++ Assets/Scripts/Mechanisms Scripts/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private bool _isOpen;
+    private Transform _doorCube;
+
+    private void Start()
+    {
+        _isOpen = false;
+        _doorCube = transform.GetChild(0);
+    }
+
+    public void Open()
+    {
+        _isOpen = true;
+        StartCoroutine(OpenAnimation());
+    }
+
+    public void Close()
+    {
+        _isOpen = false;
+        StartCoroutine(CloseAnimation());
+    }
+
+    private IEnumerator OpenAnimation()
+    {
+        while (_isOpen && Mathf.Abs(_doorCube.transform.localPosition.y - 4.25f) > 0.01f)
+        {
+            _doorCube.localPosition += Vector3.up * Time.deltaTime;
+            yield return null;
+        }
+    }
+    private IEnumerator CloseAnimation()
+    {
+        while (!_isOpen && Mathf.Abs(_doorCube.transform.localPosition.y - 1.7f) > 0.01f)
+        {
+            _doorCube.localPosition += Vector3.down * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
